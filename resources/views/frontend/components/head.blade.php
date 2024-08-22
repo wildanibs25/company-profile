18ec505b0dcc8b1726d5c7dc0362e77df6792bdb
wildanibs25 <sin990325@gmail.com>
2024-08-23 02:07:09 +0700

    modif in head and footer

--- resources/views/frontend/components/head.blade.php
+++ resources/views/frontend/components/head.blade.php
@@ -2,7 +2,7 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Online Study - Education Category Responsive Web Template - Home : W3Layouts</title>
+    <title>{{ $about->school_name }}</title>
     <!-- google-fonts -->
     <link href="//fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
     <!-- //google-fonts -->
